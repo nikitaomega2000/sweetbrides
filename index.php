93d931a198c06d20e7601e4006cfef5844f3ee60
nikitaomega2000 <nikita.omega2000@outlook.com>
2016-01-25 13:45:09 +0700

    Test

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 //	phpinfo();
 ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 $test = "Test";
 global ($test);
 	function global (&$test){
