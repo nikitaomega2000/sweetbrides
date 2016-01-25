ed2c3f1e763c388b1169489ccd8ef9a2e3e2199e
nikitaomega2000 <nikita.omega2000@outlook.com>
2016-01-25 16:32:56 +0700

    Static

--- index.php
+++ index.php
@@ -3,13 +3,21 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-$testnew = "Test";
-globalfun ($testnew);
-	function globalfun (&$test){
-	    echo "$test <br>";
-	    $test="TestNew";
-	}
-echo "$testnew <br>";
+//$testnew = "Test";
+fun ();
+fun ();
+fun ();
+fun ();
+fun ();
+fun ();
+function fun (){
+	static $a = 0;
+	echo $a++;
+}
+//	    echo "$test <br>";
+//	    $test="TestNew";
+//	}
+//echo "$testnew <br>";
 //	function inside (){
 //		echo "Внутренняя функция<br/>";
 //	}
