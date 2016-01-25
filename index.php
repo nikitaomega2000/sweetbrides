f394b9778f8e4084964462e24b4484de24e86882
Charlie Root <root@sweetbrides.ru>
2016-01-25 13:59:59 +0700

    global

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-$test = "Test";
-global ($test);
-	function global (&$test){
-
+$testnew = "Test";
+globalfun ($testnew);
+	function globalfun (&$test){
+	    echo "$test <br>";
+	    $test="TestNew";
 	}
+echo "$testnew <br>";
 //	function inside (){
 //		echo "Внутренняя функция<br/>";
 //	}
