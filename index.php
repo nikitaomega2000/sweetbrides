8e731fca67f5ddd0814921d560f9e108940b6ee7
nikitaomega2000 <nikita.omega2000@outlook.com>
2016-01-26 13:14:44 +0700

    Result test

--- index.php
+++ index.php
@@ -4,15 +4,20 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 //$testnew = "Test";
-fun ();
-fun ();
-fun ();
-fun ();
-fun ();
-fun ();
-function fun (){
-	static $a = 0;
-	echo $a++;
+fun (4);
+echo "<br/>";
+fun (5);
+echo "<br/>";
+fun (6);
+echo "<br/>";
+fun (7);
+echo "<br/>";
+fun (8);
+echo "<br/>";
+fun (9);
+function fun ($a){
+	result $a * $a;
+	echo "Test";
 }
 //	    echo "$test <br>";
 //	    $test="TestNew";
